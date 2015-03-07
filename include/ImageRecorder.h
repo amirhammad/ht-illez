1c43c5f5e26134a227a1a071bbd0f6b21d72bed0
Amir Hammad <amir.hammad@hotmail.com>
2015-03-07 10:20:58 +0100

    ImageRecorder: Added path argument to constructor of ImageRecorder
    Signed-off-by: Amir Hammad <amir.hammad@hotmail.com>

--- include/ImageRecorder.h
+++ include/ImageRecorder.h
@@ -3,10 +3,11 @@
 #include "ImageSourceOpenNI.h"
 
 #include <OpenNI.h>
+
 namespace iez {
 class ImageRecorder {
 public:
-	ImageRecorder(iez::ImageSourceOpenNI *src);
+	ImageRecorder(iez::ImageSourceOpenNI *src, const char *path);
 	~ImageRecorder();
 private:
 	openni::Recorder *m_recorder;
--- src/ImageRecorder.cpp
+++ src/ImageRecorder.cpp
@@ -1,10 +1,10 @@
 #include "ImageRecorder.h"
 using namespace openni;
 namespace iez {
-ImageRecorder::ImageRecorder(iez::ImageSourceOpenNI *src)
+ImageRecorder::ImageRecorder(iez::ImageSourceOpenNI *src, const char *path)
 {
 	m_recorder = new Recorder();
-	m_recorder->create("video.oni");
+	m_recorder->create(path);
 	m_recorder->attach(src->getColorStream(), false);
 	m_recorder->attach(src->getDepthStream(), false);
 	m_recorder->start();
