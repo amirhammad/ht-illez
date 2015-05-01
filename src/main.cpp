b032a5917742c5fcb15b3c9f5e2334c6e11af3cb
Amir Hammad <amir.hammad@hotmail.com>
2015-05-01 11:28:22 +0200

    main: cleaning unused includes
    Signed-off-by: Amir Hammad <amir.hammad@hotmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,7 @@
-
-// Custom
 #include "main.h"
-#include "ImageSourceOpenNI.h"
 #include "MainWindow.h"
 
-
-// opencv
-#include <opencv2/opencv.hpp>
-
-// STL
-#include <iostream>
-#include <string>
-
-// libfreenect
-#include <libfreenect/libfreenect.hpp>
-
-// Qt
-//#include <QThread>
 #include <QApplication>
-#include <QKeyEvent>
 
 int main(int argc, char *argv[])
 {
