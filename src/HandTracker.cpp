01f6b2ff5f5dd468779624b1e3a25f0da2b054d5
Amir Hammad <amir.hammad@hotmail.com>
2015-05-02 10:24:04 +0200

    Project: Added License headers + license GPLv3
    Signed-off-by: Amir Hammad <amir.hammad@hotmail.com>

--- src/HandTracker.cpp
+++ src/HandTracker.cpp
@@ -1,3 +1,26 @@
+/*
+ * This file is part of the project HandTrackerApp - ht-illez
+ * hosted at http://github.com/amirhammad/ht-illez
+ *
+ * Copyright (C) 2015 Amir Hammad <amir.hammad@hotmail.com>
+ *
+ *
+ * HandTrackerApp - ht-illez is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include "HandTracker.h"
 #include "Util.h"
 #include "WindowManager.h"
